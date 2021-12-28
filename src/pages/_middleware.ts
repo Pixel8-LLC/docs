d495bca4f20d5954678f40de1d0aff85e4ccaf4a
Anil Kumar M C <anirai.kumar@gmail.com>
2021-12-28 18:42:16 +0530

    fix progress

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -4,11 +4,12 @@ export function middleware(request: NextRequest) {
   if (
     request.headers.get('accept-language').startsWith('es') &&
     !request.nextUrl.pathname.includes('es')
-  )
+  ) {
     return NextResponse.redirect(`/es${request.nextUrl.pathname}`);
-  else if (
+  } else if (
     request.headers.get('accept-language').startsWith('en') &&
     !request.nextUrl.pathname.includes('en')
-  )
+  ) {
     return undefined;
+  }
 }
